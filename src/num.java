81b329bb6bea23401e5bd572eac52d1a5474b1b2
Shamna fathima.N <shamnanizam70@gmail.com>
2025-03-24 15:08:52 +0530

    basics

---
+++ src/num.java
@@ -0,0 +1,14 @@
+public class num {
+        public static void main(String args[])
+        {
+            for(int outer=1;outer<6;outer++)
+            {
+                for(int inner=1;inner<=outer;inner++)
+                {
+                    System.out.print(outer);
+                }
+                System.out.println();
+            }
+        }
+
+    }
