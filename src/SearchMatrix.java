d885a52407920627b6574e3c31947ef9d0dccab4
Shamna fathima.N <shamnanizam70@gmail.com>
2025-05-16 16:12:16 +0530

    basics

---
+++ src/SearchMatrix.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SearchMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of cols: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("Enter target to search: ");
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        // Search for the target
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+
+        // Output resultSystem.out.print("Enter number of rows: ");
+        if (found) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not Found");
+        }
+
+        sc.close();
+    }
+}
